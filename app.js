d6cdd4fabc4760561a2604e233bd9d3ed34984f0
sm03 <smalbranche10@gmail.com>
2026-02-25 22:24:05 +0100

    Middleware chaîné et résilience serveur

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ const express = require('express');
 const app = express();
 
 // Sans cette call-back, le serveur génèrerait une erreur 404
-app.use((req, res) => {
+// Avec l'ajout de next => chaînage des "middleware" Express
+app.use((req, res, next) => {
     res.json({ message : 'Votre requête a bien été reçue !'});
+    console.log('Requête reçue');
+    next();
+});
+
+app.use((req, res, next) => {
+    res.status(201);
+    next(); // pour chaînage
+});
+
+app.use((req, res, next) => {
+    console.log('Réponse envoyée avec succès !');
 });
 
 module.exports = app;
